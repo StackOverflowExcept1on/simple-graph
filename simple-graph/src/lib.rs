a3ecff33b9db3d940cbf0d49e9050b0c753d4d8d
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-01-09 10:16:45 +0300

    added crate level documentation

--- simple-graph/src/lib.rs
+++ simple-graph/src/lib.rs
@@ -1,3 +1,11 @@
+//! Graph library with ability to serialize/deserialize Trivial Graph Format
+//!
+//! Besides serialize/deserialize library can deal with graph algorithms such
+//! as [Depth-first search (DFS)](https://en.wikipedia.org/wiki/Depth-first_search)
+//! and [Breadth-first search (BFS)](https://en.wikipedia.org/wiki/Breadth-first_search).
+//!
+//! If you are looking for example visit [`Graph`]
+
 #![feature(str_split_whitespace_remainder)]
 
 pub use error::*;
